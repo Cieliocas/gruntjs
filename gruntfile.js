37a9c5f3422bb6f4ecb1c21170d43fe30f03ab69
Cieliocas <francielioevangelista@hotmail.com>
2024-11-01 06:00:04 -0300

    start new project

--- gruntfile.js
+++ gruntfile.js
@@ -1,55 +1,25 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-        pkg: grunt.file.readJSON("package.json"),
+        pkg: grunt.file.readJSON('package.json'),
         less: {
             development: {
                 files: {
-                    "styles.css": "styles.less"
-                },
+                    'dev/styles/styles.css': 'src/styles/styles.less'
+                }
             },
             production: {
                 options: {
                     compress: true,
                 },
                 files: {
-                    "styles.min.css": "styles.less"
+                    'dist/styles/styles.min.css': 'src/styles/styles.less'
                 }
             }
         },
-        sass: {
-            dist: {
-                options: {
-                    style: "compressed"
-                },
-                files: {
-                    "styles2.css": "styles.scss"
-                }
-            }
-        },
-        concurrent: {
-            target: ['olaGrunt', 'less', 'sass', 'tarefaDemorada']
-        }
-    });
+    })
 
-    grunt.registerTask('olaGrunt', function() {
-        const done = this.async();
-        setTimeout(function() {
-            console.log("Hello, Grunt!");
-            done();
-        }, 3000);
-    });
+    grunt.loadNpmTasks('grunt-contrib-less');
 
-    grunt.registerTask('tarefaDemorada', function() {
-        const done = this.async();
-        setTimeout(function() {
-            console.log("Hello, Grunt!");
-            done();
-        }, 3000);
-    });
-
-    grunt.loadNpmTasks("grunt-contrib-less");
-    grunt.loadNpmTasks("grunt-contrib-sass");
-    grunt.loadNpmTasks("grunt-concurrent");
-
-    grunt.registerTask('default', ['concurrent']);
+    grunt.registerTask('default', ['less:development']);
+    grunt.registerTask('build', ['less:production']);
 }
