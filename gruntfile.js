a0f59247b63b5361f1f7b2524d98b08f982ab1b1
Cieliocas <francielioevangelista@hotmail.com>
2024-11-03 23:33:17 -0300

    instal and use watch on gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function(grunt) {
         watch: {
             less: {
                 files: ['src/styles/**/*.less'],
-                task: ['less:development']
+                tasks: ['less:development']
             }
         }
     })
