a429a2cc82cf81187003daa8c3a0b9923960e09f
Cieliocas <francielioevangelista@hotmail.com>
2024-11-03 23:50:18 -0300

    replacement part 1

--- gruntfile.js
+++ gruntfile.js
@@ -21,11 +21,32 @@ module.exports = function(grunt) {
                 files: ['src/styles/**/*.less'],
                 tasks: ['less:development']
             }
+        },
+        replace: {
+            dev: {
+                options: {
+                    patterns: [
+                        {
+                            match: 'ENDERECO_DO_CSS',
+                            replacement: './styles/styles.css'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['dev/index.html'],
+                        dest: 'dev/'
+                    }
+                ]
+            }
         }
     })
 
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-replace');
 
     grunt.registerTask('default', ['watch']);
     grunt.registerTask('build', ['less:production']);
